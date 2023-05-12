350e78e15c35ad7b699791adefb060d9aeba04a0
Ahusan <ahusan5657@gmail.com>
2023-05-12 16:54:30 +0500

    Added env and read me

---
+++ README.md
@@ -0,0 +1,27 @@
+# Python Script to scrape [Mihaaru](https://mihaaru.com/news/) article headings
+This is a project to scrape the headings of new articles on mihaaru. You can change this code to scrape any other website of your choosing if it the URL can be looped. Just change the URL from env file. And change the finding tags and classes accordingly. 
+
+## This project is made using Python version : 3.9.12
+
+## Clone this project
+```
+git clone https://github.com/ahusan/py-web-scrapper.git
+```
+
+## CD to project
+```
+cd ./py-web-scrapper
+```
+
+## Install Requirements
+```
+pip install -r requirements.txt
+```
+## Rename .env.example to .env
+
+## Run 
+```
+python ./app.py
+```
+## For better readability align text like so
+![Notepad file](/notepage.png)
--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 import csv
+from dotenv import load_dotenv
 
-# Making a GET request
-url = 'https://mihaaru.com/news/'
+load_dotenv()
 
-# Parsing the HTML
 
-# s = soup.find('div', class_='entry-content')
 
-# lines = s.find_all('p')
+
+# Settign variables from env file
+rangefrom = int(os.getenv('RANGE_FROM'))
+rangeto = int(os.getenv('RANGE_TO'))
+url = os.getenv('SCRAPE_URL')
+tag = os.getenv('SCRAPE_TAG')
+date_tag = os.getenv('SCRAPE_DATE_TAG')
+class_name = os.getenv('SCRAPE_CLASS')
+date_class = os.getenv('SCRAPE_DATE_CLASS')
+#File to save the headings
 filename = 'titles.csv'
 
-
+#Setting up a CSV file to save the article Headings
 with open(filename, 'w', newline='', encoding='utf-8') as f:
-    fieldnames = ['Page', 'Title']
-    w = csv.DictWriter(f, fieldnames=fieldnames)
+    fieldnames = ['Page', 'Date', 'Title']
+    w = csv.DictWriter(f, fieldnames=fieldnames, delimiter='|', dialect='excel')
     w.writeheader()
-    for page in range(10001, 10200):
+    
+    #Looping through the first 200 articles
+    for page in range(rangefrom, rangeto):
         req = requests.get(url + str(page) + '/')
         soup = BeautifulSoup(req.content, 'html.parser')
 
-        title = soup.find('h1', class_='text-waheed')
+        title = soup.find(tag, class_=class_name);
+        date = soup.find(date_tag, class_=date_class);
+        #Checking if an article exist
         if(title):
-            d = {}
-            print(page, title.text)
-            w.writerow({'Page':page, 'Title':title.text})
+            print(page, date.text, title.text)
+            w.writerow({'Page':page, 'Date': date.text, 'Title':title.text})
         else:
-            print('Article does not exist')
+            print(page, 'Article does not exist')
         
         
 
